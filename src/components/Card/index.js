114f0ffb38c75e71d2780312c59a6d99f9439178
Sheena Brevig <sheena.brevig@gmail.com>
2019-08-11 18:00:33 -0700

    got images to load properly

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,19 +10,7 @@ function Card(props) {
         <div className="img-container">
           <img alt={props.name} src={props.image} />
         </div>
-        <div className="content">
-          <ul>
-            <li>
-              <strong>Name:</strong> {props.name}
-            </li>
-            <li>
-              <strong>Occupation:</strong> {props.occupation}
-            </li>
-            <li>
-              <strong>Address:</strong> {props.location}
-            </li>
-          </ul>
-        </div>
+        
         <span className="remove" onClick= {() => props.deleteFriend(props.id)} >𝘅</span>
       </div>
     );
